ed14f17d33c544097f4f3b75218f1bfcec122656
Thiago <gutierrezdelatorrej30@gmail.com>
2025-03-14 23:07:15 -0500

    add folder

---
+++ store_project/model/ModelData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store_project.model
+{
+    class ModelData
+    {
+    }
+}
--- store_project/view/FormMain.cs
+++ store_project/view/FormMain.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace store_project
+namespace store_project.view
 {
-    public partial class Form1 : Form
+    public partial class FormMain: Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
         }
